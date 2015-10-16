58ffaf790a44462a3efd85a28f1a86083a59f6f0
Kimiko Farmer <kimikof@uw.edu>
2015-10-15 17:11:23 -0700

    completed class work

--- jquery/js/app.js
+++ jquery/js/app.js
@@ -6,3 +6,23 @@
     http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f
 */
 
+//when the DOM centent has been loaded...
+$(function() {
+	'use strict';
+	$('a').attr('target', '_blank');
+	$('article').hide().fadeIn();
+	$('#toggle-article').click(function() {
+		$('article').fadeToggle();
+	});
+
+	var url = 'http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f'
+	$.getJSON(url)
+		.then(function(data) {
+			console.log(data);
+			var temperature = data.main.temp;
+			$('#temp').text(temperature);
+		});
+
+		console.log('test');
+
+});
